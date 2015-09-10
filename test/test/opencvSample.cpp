3d578b960194af151bc746b05dc2907949e8f75d
wangjinjin <wangjinjin@dtvlab.cn>
2015-09-10 16:01:27 +0800

    1. opencv的配置属性  2.访问像素例子

--- test/test/opencvSample.cpp
+++ test/test/opencvSample.cpp
@@ -1,8 +1,62 @@
-int main()
-{
+#include <stdio.h>
+#include <opencv2\opencv.hpp>  
+#include <iostream>  
+#include <string>  
+using namespace cv;  
+using namespace std;  
+
+int main()  
+{  
+#if 0
+  printf("hell\n");
+  FILE *f = NULL;
+  f =  fopen("D:\liu.jpg", "r");
+  if(f == NULL)
+  {
+	    printf("hell========\n");
+  }
+  else
+  {
+	 fclose(f);
+  }
+  getchar();
+#endif
+#if 0
+  Mat img = imread("D:\\li.jpg", 0);
+
+  if(img.empty())
+  {
+	   cout<<"error";  
+	   return -1;
+
+  }
+  printf("%d\n",img.channels());
+  cout<<"oooo";
+  imshow("mypic", img);
+#endif
+
+  Mat grayimg(300,200,CV_8UC1);
+
+  imshow("mypic", grayimg); 
+  
+  
+  waitKey(0);
 
 
+ // Mat img(300,200,CV_8UC3,Scalar(0,0,255));
+  for(int i = 0; i < grayimg.rows; i++)
+  {
 
+	   uchar *p = grayimg.ptr<uchar>(i);
+	   for(int j = 0; j < grayimg.cols; j++)
+	   {
+		   p[j] = 255;
 
-	return 0;
-}
+	   }
+
+  }
+
+  imshow("mypic", grayimg);
+  waitKey(0);
+  return 0;  
+}  
